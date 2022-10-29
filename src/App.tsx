18594944f7736cd44944e6ab800f81f8db68aaa5
Nelio Alves <acenelio@gmail.com>
2022-10-29 17:25:26 -0300

    Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  
+
   return (
-    <h1>Bookkeeper!</h1>
+    <nav
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Link to="/invoices">Invoices</Link> |{" "}
+      <Link to="/expenses">Expenses</Link>
+    </nav>
   )
 }
 
