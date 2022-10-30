fda7904a12b9fca0811b79eea815bafa6ed4152e
Nelio Alves <acenelio@gmail.com>
2022-10-30 09:32:25 -0300

    Listing invoices

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import { getInvoices } from "../../data";
+
 export default function Invoices() {
+
+    let invoices = getInvoices();
+
     return (
-        <main style={{ padding: "1rem 0" }}>
-            <h2>Invoices</h2>
-        </main>
+        <div style={{ display: "flex" }}>
+            <nav
+                style={{
+                    borderRight: "solid 1px",
+                    padding: "1rem",
+                }}
+            >
+                {invoices.map((invoice) => (
+                    <Link
+                        style={{ display: "block", margin: "1rem 0" }}
+                        to={`/invoices/${invoice.number}`}
+                        key={invoice.number}
+                    >
+                        {invoice.name}
+                    </Link>
+                ))}
+            </nav>
+        </div>
     );
 }
