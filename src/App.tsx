07b29411dda0064b4359ea1dc2674f96cf844f39
Nelio Alves <acenelio@gmail.com>
2022-10-30 09:19:58 -0300

    Nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <nav
-      style={{
-        borderBottom: "solid 1px",
-        paddingBottom: "1rem",
-      }}
-    >
-      <Link to="/invoices">Invoices</Link> |{" "}
-      <Link to="/expenses">Expenses</Link>
-    </nav>
+    <>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,10 @@ import Invoices from './routes/Invoices'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} />
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 )
